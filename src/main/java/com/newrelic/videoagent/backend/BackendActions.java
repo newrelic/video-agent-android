31e8fab80da169fbc900ca2e9886e1e96b402171
Andreu Santaren Llop <asantaren@newrelic.com>
2018-09-18 15:19:18 +0200

    Update EventDefs and make it static strings.

--- src/main/java/com/newrelic/videoagent/backend/BackendActions.java
+++ src/main/java/com/newrelic/videoagent/backend/BackendActions.java
@@ -31,7 +31,7 @@ public class BackendActions {
         // TODO: implement action specific options
 
         if (NewRelic.currentSessionId() != null) {
-            NewRelic.recordCustomEvent("VideoEvent", attr);
+            NewRelic.recordCustomEvent(EventDefs.VIDEO_EVENT, attr);
         }
         else {
             AVLog.e("⚠️ The NewRelicAgent is not initialized, you need to do it before using the NewRelicVideo. ⚠️");
@@ -43,126 +43,126 @@ public class BackendActions {
     // Contents senders
 
     public void sendRequest() {
-        sendAction(EventDefs.CONTENT_REQUEST.toString());
+        sendAction(EventDefs.CONTENT_REQUEST);
     }
 
     public void sendStart() {
-        sendAction(EventDefs.CONTENT_START.toString());
+        sendAction(EventDefs.CONTENT_START);
     }
 
     public void sendEnd() {
-        sendAction(EventDefs.CONTENT_END.toString());
+        sendAction(EventDefs.CONTENT_END);
     }
 
     public void sendPause() {
-        sendAction(EventDefs.CONTENT_PAUSE.toString());
+        sendAction(EventDefs.CONTENT_PAUSE);
     }
 
     public void sendResume() {
-        sendAction(EventDefs.CONTENT_RESUME.toString());
+        sendAction(EventDefs.CONTENT_RESUME);
     }
 
     public void sendSeekStart() {
-        sendAction(EventDefs.CONTENT_SEEK_START.toString());
+        sendAction(EventDefs.CONTENT_SEEK_START);
     }
 
     public void sendSeekEnd() {
-        sendAction(EventDefs.CONTENT_SEEK_END.toString());
+        sendAction(EventDefs.CONTENT_SEEK_END);
     }
 
     public void sendBufferStart() {
-        sendAction(EventDefs.CONTENT_BUFFER_START.toString());
+        sendAction(EventDefs.CONTENT_BUFFER_START);
     }
 
     public void sendBufferEnd() {
-        sendAction(EventDefs.CONTENT_BUFFER_END.toString());
+        sendAction(EventDefs.CONTENT_BUFFER_END);
     }
 
     public void sendHeartbeat() {
-        sendAction(EventDefs.CONTENT_HEARTBEAT.toString());
+        sendAction(EventDefs.CONTENT_HEARTBEAT);
     }
 
     public void sendRenditionChange() {
-        sendAction(EventDefs.CONTENT_RENDITION_CHANGE.toString());
+        sendAction(EventDefs.CONTENT_RENDITION_CHANGE);
     }
 
     public void sendError() {
-        sendAction(EventDefs.CONTENT_ERROR.toString());
+        sendAction(EventDefs.CONTENT_ERROR);
     }
 
     // Ads senders
 
     public void sendAdRequest() {
-        sendAction(EventDefs.AD_REQUEST.toString());
+        sendAction(EventDefs.AD_REQUEST);
     }
 
     public void sendAdStart() {
-        sendAction(EventDefs.AD_START.toString());
+        sendAction(EventDefs.AD_START);
     }
 
     public void sendAdEnd() {
-        sendAction(EventDefs.AD_END.toString());
+        sendAction(EventDefs.AD_END);
     }
 
     public void sendAdPause() {
-        sendAction(EventDefs.AD_PAUSE.toString());
+        sendAction(EventDefs.AD_PAUSE);
     }
 
     public void sendAdResume() {
-        sendAction(EventDefs.AD_RESUME.toString());
+        sendAction(EventDefs.AD_RESUME);
     }
 
     public void sendAdSeekStart() {
-        sendAction(EventDefs.AD_SEEK_START.toString());
+        sendAction(EventDefs.AD_SEEK_START);
     }
 
     public void sendAdSeekEnd() {
-        sendAction(EventDefs.AD_SEEK_END.toString());
+        sendAction(EventDefs.AD_SEEK_END);
     }
 
     public void sendAdBufferStart() {
-        sendAction(EventDefs.AD_BUFFER_START.toString());
+        sendAction(EventDefs.AD_BUFFER_START);
     }
 
     public void sendAdBufferEnd() {
-        sendAction(EventDefs.AD_BUFFER_END.toString());
+        sendAction(EventDefs.AD_BUFFER_END);
     }
 
     public void sendAdHeartbeat() {
-        sendAction(EventDefs.AD_HEARTBEAT.toString());
+        sendAction(EventDefs.AD_HEARTBEAT);
     }
 
     public void sendAdRenditionChange() {
-        sendAction(EventDefs.AD_RENDITION_CHANGE.toString());
+        sendAction(EventDefs.AD_RENDITION_CHANGE);
     }
 
     public void sendAdError() {
-        sendAction(EventDefs.AD_ERROR.toString());
+        sendAction(EventDefs.AD_ERROR);
     }
 
     public void sendAdBreakStart() {
-        sendAction(EventDefs.AD_BREAK_START.toString());
+        sendAction(EventDefs.AD_BREAK_START);
     }
 
     public void sendAdBreakEnd() {
-        sendAction(EventDefs.AD_BREAK_END.toString());
+        sendAction(EventDefs.AD_BREAK_END);
     }
 
     public void sendAdQuartile() {
-        sendAction(EventDefs.AD_QUARTILE.toString());
+        sendAction(EventDefs.AD_QUARTILE);
     }
 
     public void sendAdClick() {
-        sendAction(EventDefs.AD_CLICK.toString());
+        sendAction(EventDefs.AD_CLICK);
     }
 
     // Misc senders
 
     public void sendPlayerReady() {
-        sendAction(EventDefs.PLAYER_READY.toString());
+        sendAction(EventDefs.PLAYER_READY);
     }
 
     public void sendDownload() {
-        sendAction(EventDefs.DOWNLOAD.toString());
+        sendAction(EventDefs.DOWNLOAD);
     }
 }
