d585737588303565ad5fea205b491ff072ec3084
Andreu Santaren Llop <asantaren@newrelic.com>
2018-11-15 17:54:57 +0100

    Implement registerGetter CAL and callback dictionary.

--- src/main/java/com/newrelic/videoagent/CAL.java
+++ src/main/java/com/newrelic/videoagent/CAL.java
@@ -1,11 +1,27 @@
 package com.newrelic.videoagent;
 
+import android.util.Pair;
 import com.newrelic.agent.android.NewRelic;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CAL {
+
+    private Map<Long, Map<String, Pair<Object, Method>>> callbacksTree;
+
+    private static CAL instance = null;
+
+    protected CAL() {
+        callbacksTree = new HashMap<>();
+    }
+
+    public static CAL getInstance() {
+        if (instance == null) {
+            instance = new CAL();
+        }
+        return instance;
+    }
 
     public static void recordCustomEvent(String name, Map attr) {
         if (NewRelic.currentSessionId() != null) {
@@ -35,5 +51,35 @@ public class CAL {
 
     public static void AV_LOG(String str) {
         NRLog.d(str);
+    }
+
+    public static Object callGetter(String name, Long pointerToOrigin) {
+
+        // TODO: find callback, execute and return result
+
+        Object target = null;
+        Method method = null;
+
+        try {
+            Object ret = method.invoke(target);
+        }
+        catch (Exception e) { }
+
+        return null;
+    }
+
+    public static void registerGetter(String name, Object target, Method method, Long pointerToOrigin) {
+
+        // Get callbacks for object with address "pointerToOrigin"
+        Map<String, Pair<Object, Method>> callbacks = getInstance().callbacksTree.get(pointerToOrigin);
+
+        // If no callbacks yet, create an empty hashmap
+        if (callbacks == null) {
+            getInstance().callbacksTree.put(pointerToOrigin, new HashMap<String, Pair<Object, Method>>());
+            callbacks = getInstance().callbacksTree.get(pointerToOrigin);
+        }
+
+        // Store the new callback indexed by "name" (getter name)
+        callbacks.put(name, new Pair<>(target, method));
     }
 }
