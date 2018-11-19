b58b4f82130e72e1e75faba3f252b4d73e0adeba
Andreu Santaren Llop <asantaren@newrelic.com>
2018-11-19 16:55:17 +0100

    Register getters.

--- src/main/java/com/newrelic/videoagent/tracker/AdsTracker.java
+++ src/main/java/com/newrelic/videoagent/tracker/AdsTracker.java
@@ -9,7 +9,54 @@ public class AdsTracker extends AdsTrackerCore {
     public AdsTracker() {
         super();
 
-        // TODO: register callbacks
+        registerGetter("numberOfAds", "getNumberOfAdsAttr");
+        registerGetter("trackerName", "getTrackerName");
+        registerGetter("trackerVersion", "getTrackerVersion");
+        registerGetter("playerVersion", "getPlayerVersion");
+        registerGetter("playerName", "getPlayerName");
+        registerGetter("isAd", "getIsAd");
+
+        registerGetter("adId", "getVideoId");
+        registerGetter("adTitle", "getTitle");
+        registerGetter("adBitrate", "getBitrate");
+        registerGetter("adRenditionName", "getRenditionName");
+        registerGetter("adRenditionBitrate", "getRenditionBitrate");
+        registerGetter("adRenditionWidth", "getRenditionWidth");
+        registerGetter("adRenditionHeight", "getRenditionHeight");
+        registerGetter("adDuration", "getDuration");
+        registerGetter("adPlayhead", "getPlayhead");
+        registerGetter("adLanguage", "getLanguage");
+        registerGetter("adSrc", "getSrc");
+        registerGetter("adIsMuted", "getIsMuted");
+        registerGetter("adCdn", "getCdn");
+        registerGetter("adFps", "getFps");
+        registerGetter("adCreativeId", "getAdCreativeId");
+        registerGetter("adPosition", "getAdPosition");
+        registerGetter("adPartner", "getAdPartner");
+    }
+
+    public Object getNumberOfAdsAttr() {
+        return new Long((long)getNumberOfAds());
+    }
+
+    public Object getIsAd() {
+        throw new RuntimeException("getIsAd must be overwritten by subclass");
+    }
+
+    public Object getPlayerName() {
+        throw new RuntimeException("getPlayerName must be overwritten by subclass");
+    }
+
+    public Object getPlayerVersion() {
+        throw new RuntimeException("getPlayerVersion must be overwritten by subclass");
+    }
+
+    public Object getTrackerName() {
+        throw new RuntimeException("getTrackerName must be overwritten by subclass");
+    }
+
+    public Object getTrackerVersion() {
+        throw new RuntimeException("getTrackerVersion must be overwritten by subclass");
     }
 
     protected void registerGetter(String name, String methodName) {
