7f0866fb9f33c64970d216f0beb2cb42518879eb
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-03-04 15:19:49 +0100

    Create empty module and modify example.

---
+++ app/src/main/java/com/newrelic/nrvideoproject/VideoPlayerAds.java
@@ -0,0 +1,85 @@
+package com.newrelic.nrvideoproject;
+
+import android.os.Bundle;
+
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.newrelic.videoagent.core.NewRelicVideoAgent;
+import com.newrelic.videoagent.core.tracker.NRVideoTracker;
+import com.newrelic.videoagent.exoplayer.tracker.NRTrackerExoPlayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VideoPlayerAds extends AppCompatActivity {
+
+    private SimpleExoPlayer player;
+    private Integer trackerId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player_ads);
+
+        String video = getIntent().getStringExtra("video");
+
+        if (video.equals("Tears")) {
+            Log.v("VideoPlayer", "Play Tears");
+            playVideo("http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd");
+        }
+        else if (video.equals("Playhouse")) {
+            Log.v("VideoPlayer", "Play Playhouse");
+            playVideo("https://bitmovin-a.akamaihd.net/content/playhouse-vr/mpds/105560.mpd");
+        }
+        else if (video.equals("Kite")) {
+            Log.v("VideoPlayer", "Play Kite");
+            playVideo("https://demos.transloadit.com/dashtest/my_playlist.mpd");
+        }
+        else if (video.equals("Live")) {
+            Log.v("VideoPlayer", "Play Live");
+            playVideo("https://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd");
+        }
+        else {
+            Log.v("VideoPlayer","Unknown video");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ((NRVideoTracker) NewRelicVideoAgent.getInstance().getContentTracker(trackerId)).sendEnd();
+        NewRelicVideoAgent.getInstance().releaseTracker(trackerId);
+    }
+
+    private void playVideo(String videoUrl) {
+        player = new SimpleExoPlayer.Builder(this).build();
+
+        NRTrackerExoPlayer tracker = new NRTrackerExoPlayer();
+
+        trackerId = NewRelicVideoAgent.getInstance().start(tracker);
+
+        Map<String, Object> attr = new HashMap<>();
+        attr.put("myAttrStr", "Hello");
+        attr.put("myAttrInt", 101);
+        tracker.sendEvent("CUSTOM_ACTION", attr);
+
+        PlayerView playerView = findViewById(R.id.player);
+        playerView.setPlayer(player);
+
+        tracker.setPlayer(player);
+
+        player.setMediaItem(MediaItem.fromUri(videoUrl));
+        // Prepare the player.
+        player.setPlayWhenReady(true);
+        player.prepare();
+    }
+}
