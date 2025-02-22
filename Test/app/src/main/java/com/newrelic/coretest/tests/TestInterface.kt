58e8c59d58c63c1c9a66a9710caf1b66cee916ca
mavinash <mavinash@newrelic.com>
2025-02-22 12:27:48 +0530

    feat: Clear for app

--- Test/app/src/main/java/com/newrelic/coretest/tests/TestInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.newrelic.coretest.tests
-
-import com.newrelic.videoagent.core.NewRelicVideoAgent
-import com.newrelic.videoagent.core.tracker.NRVideoTracker
-
-interface TestInterface {
-    fun doTest(callback: (String, Boolean) -> Unit)
-}
