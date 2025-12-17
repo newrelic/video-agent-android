b4c11fa1ffad575d2f3806e9c059879fad190f42
Divya Latha <dsreenivasulu@newrelic.com>
2025-12-17 15:43:48 +0530

    chore: configure production release automation

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -11,7 +11,6 @@ fi
 echo "Bumping version to $NEW_VERSION in gradle.properties..."
 
 # Use sed to find the line starting with GLOBAL_VERSION_NAME and replace it
-# This syntax works on both Linux (GitHub Actions) and Mac (Locally)
 sed -i.bak "s/^GLOBAL_VERSION_NAME=.*/GLOBAL_VERSION_NAME=$NEW_VERSION/" gradle.properties
 
 # Remove the backup file created by sed
