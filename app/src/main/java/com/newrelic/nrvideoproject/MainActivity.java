0cfa54a9f850b51f32a251750a85b174007e2a99
swatijha <swatijha@newrelic.com>
2026-02-05 11:47:11 +0530

    NR-517762: feat: add toggle feature for QOE

--- app/src/main/java/com/newrelic/nrvideoproject/MainActivity.java
+++ app/src/main/java/com/newrelic/nrvideoproject/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Switch;
 
 import com.newrelic.videoagent.core.NRVideo;
 import com.newrelic.videoagent.core.NRVideoConfiguration;
+import com.newrelic.videoagent.core.config.NRVideoConfig;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .enableQoeAggregate(BuildConfig.QOE_AGGREGATE_DEFAULT)
                 .build();
         NRVideo.newBuilder(getApplicationContext()).withConfiguration(config).build();
+        // Initialize QOE configuration with value from local.properties
+        NRVideoConfig.getInstance().initializeDefaults(BuildConfig.QOE_AGGREGATE_DEFAULT);
         setContentView(R.layout.activity_main);
 
         adsSwitch = findViewById(R.id.ads_switch);
