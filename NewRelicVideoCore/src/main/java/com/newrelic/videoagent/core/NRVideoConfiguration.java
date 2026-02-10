330e924f4aa8c4ba92b19c831eef7a77881c834d
swatijha <swatijha@newrelic.com>
2026-02-10 18:06:53 +0530

    rebased with stable-beta

--- NewRelicVideoCore/src/main/java/com/newrelic/videoagent/core/NRVideoConfiguration.java
+++ NewRelicVideoCore/src/main/java/com/newrelic/videoagent/core/NRVideoConfiguration.java
@@ -203,6 +203,8 @@ public final class NRVideoConfiguration {
         private boolean memoryOptimized = true;
         private boolean debugLoggingEnabled = false;
         private boolean isTV = false;
+        private String collectorAddress = null;
+        private boolean qoeAggregateEnabled = true; // Default enabled
 
         public Builder(String applicationToken) {
             this.applicationToken = applicationToken;
